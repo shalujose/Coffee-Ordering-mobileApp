0ad42159089021cb2cfca0327ffe32f5a81cea58
shalujose <35284680+shalujose@users.noreply.github.com>
2020-05-07 17:25:06 +0530

    Reformatted code committed version2.0

--- src/main/java/com/revature/myeventapplication/MainActivity.java
+++ src/main/java/com/revature/myeventapplication/MainActivity.java
@@ -18,8 +18,8 @@ import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button button,order,addCoffee,minusCoffee,redirect;
-    private int noOfCoffee=2,price;
+    private Button button, order, addCoffee, minusCoffee, redirect;
+    private int noOfCoffee = 2, price;
     private boolean hasWhippedCream;
     private CheckBox whippedCream;
 
@@ -29,18 +29,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        redirect=(Button) findViewById(R.id.button2);
+        redirect = (Button) findViewById(R.id.button2);
         whippedCream = (CheckBox) findViewById(R.id.checkBox);
 
-        addCoffee=(Button) findViewById(R.id.button3);
-        addCoffee.setOnClickListener(new View.OnClickListener(){
+        addCoffee = (Button) findViewById(R.id.button3);
+        addCoffee.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 incrementOrder();
             }
         });
 
-        minusCoffee=(Button) findViewById(R.id.button4);
+        minusCoffee = (Button) findViewById(R.id.button4);
         minusCoffee.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -48,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        order=(Button) findViewById(R.id.button);
+        order = (Button) findViewById(R.id.button);
         order.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -57,7 +57,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        button=(Button) findViewById(R.id.button2);
+        button = (Button) findViewById(R.id.button2);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -66,62 +66,62 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public void incrementOrder(){
-        if(noOfCoffee==100){
-            Toast.makeText(this,"You can't make order above 100",Toast.LENGTH_SHORT).show();
+    public void incrementOrder() {
+        if (noOfCoffee == 100) {
+            Toast.makeText(this, "You can't make order above 100", Toast.LENGTH_SHORT).show();
         }
-        noOfCoffee=noOfCoffee+1;
+        noOfCoffee = noOfCoffee + 1;
         display(noOfCoffee);
     }
 
-    public void decrementOrder(){
-        if(noOfCoffee>1)
-        noOfCoffee=noOfCoffee-1;
+    public void decrementOrder() {
+        if (noOfCoffee > 1)
+            noOfCoffee = noOfCoffee - 1;
         display(noOfCoffee);
     }
 
-    public void submitOrder(View view){
-        EditText text=(EditText) findViewById(R.id.editText3);
-        String nametext=text.getText().toString();
+    public void submitOrder(View view) {
+        EditText text = (EditText) findViewById(R.id.editText3);
+        String nametext = text.getText().toString();
 
-        whippedCream=(CheckBox) findViewById(R.id.checkBox);
-        hasWhippedCream=whippedCream.isChecked();
-        int price = displayPrice(noOfCoffee*5,nametext,hasWhippedCream);
-        Log.v("MainActivity","whipped cream checkbox: "+hasWhippedCream);
-        orderSummery(nametext,price,hasWhippedCream);
+        whippedCream = (CheckBox) findViewById(R.id.checkBox);
+        hasWhippedCream = whippedCream.isChecked();
+        int price = displayPrice(noOfCoffee * 5, nametext, hasWhippedCream);
+        Log.v("MainActivity", "whipped cream checkbox: " + hasWhippedCream);
+        orderSummery(nametext, price, hasWhippedCream);
 
-        Intent intent =new Intent(Intent.ACTION_SENDTO);
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
         intent.setData(Uri.parse("mailto: "));
-        intent.putExtra(Intent.EXTRA_SUBJECT,"Coffee Ordering "+nametext);
-        intent.putExtra(Intent.EXTRA_TEXT,""+orderSummery(nametext,price,hasWhippedCream));
-        if(intent.resolveActivity(getPackageManager())!=null){
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Coffee Ordering " + nametext);
+        intent.putExtra(Intent.EXTRA_TEXT, "" + orderSummery(nametext, price, hasWhippedCream));
+        if (intent.resolveActivity(getPackageManager()) != null) {
             startActivity(intent);
         }
 
     }
 
     private void display(int orderNo) {
-        TextView oderView =(TextView) findViewById(R.id.textView2);
-        oderView.setText(""+orderNo);
+        TextView oderView = (TextView) findViewById(R.id.textView2);
+        oderView.setText("" + orderNo);
     }
 
-    private int displayPrice(int coffeePrice, String name,boolean hasWhippedCream) {
-        TextView priceView= (TextView) findViewById(R.id.textView3);
+    private int displayPrice(int coffeePrice, String name, boolean hasWhippedCream) {
+        TextView priceView = (TextView) findViewById(R.id.textView3);
         /*priceView.setText(NumberFormat.getCurrencyInstance().format(coffeePrice));*/
-        priceView.setText(orderSummery(name,coffeePrice,hasWhippedCream));
+        priceView.setText(orderSummery(name, coffeePrice, hasWhippedCream));
         return coffeePrice;
     }
 
-    public void openSecondActivity(){
-        Intent intent=new Intent(this,Main2Activity.class);
+    public void openSecondActivity() {
+        Intent intent = new Intent(this, Main2Activity.class);
         startActivity(intent);
     }
 
-    private String orderSummery(String name,int price,boolean whippedCream){
-        String message= "Name: "+name;
-        message+="\nQuantity: "+noOfCoffee;
-        message+="\nWhipped cream: "+whippedCream;
-        message+="\nTotal: $"+price;
+    private String orderSummery(String name, int price, boolean whippedCream) {
+        String message = "Name: " + name;
+        message += "\nQuantity: " + noOfCoffee;
+        message += "\nWhipped cream: " + whippedCream;
+        message += "\nTotal: $" + price;
         return message;
     }
 }
